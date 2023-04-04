cbf8ebef619d47f388149e3f6665443f5e64d62d
Sulin <eparis@atlassian.com>
2023-04-04 19:12:43 +0300

    New commit3

--- task_1/solution.js
+++ task_1/solution.js
@@ -34,7 +34,6 @@ function addInCartSumChange(sum, delta) {
     // Задание №1.3. Увеличение суммы в корзине
 
     let newSum = sum + delta; // создайте переменную newSum и присвойте ей сумму oldSum и difference
-    let newSumText = sum + delta + ' ₽'; // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
     let newSumText = `${newSumText} ₽`; // замените склеивание, используемое в newSum Text на шаблонную строку
 
     // Конец решения задания №1.3.
