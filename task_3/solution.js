daa070c569e599644b0935122475da9e9194dd6a
Sulin <eparis@atlassian.com>
2023-04-17 11:56:54 +0300

    task 3.1 done

--- task_3/solution.js
+++ task_3/solution.js
@@ -4,7 +4,7 @@ function renderCartItem(item) {
     // Задание №3.1. Формирование строки корзины
    
    let sum = item.count * item.price;
-   let itemCountText = '' + item.count + ' * ' + item.price + ' ₽ = ' + sum + ' ₽';
+   let itemCountText = '' + item.count + ' × ' + item.price + ' ₽ = ' + sum + ' ₽';
 
     // Конец решения задания №3.1.
 
