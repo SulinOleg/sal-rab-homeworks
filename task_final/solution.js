8b07342d391c6ddea66f939bfaee5fe07723d372
Sulin <eparis@atlassian.com>
2023-05-01 10:11:16 +0300

    final task v4

--- task_final/solution.js
+++ task_final/solution.js
@@ -4,14 +4,16 @@ function sendRequest(name, phone, address, goods, sum) {
   
   let countOfGoods = goods.length;
 
-  let goods = {title, count};
-
   for (let i = 0; i < countOfGoods; i += 1) {
+        data.goods =[{
+          title: [], 
+          count: []
+        }];
         data.goods.push(goods[i].title);
         data.goods.push(goods[i].count);
     }
 
-    address = "" + "ул. " + address.street + "," + "дом  " + address.house + "," + address.entrance + " подъезд" + "," + address.floor + " этаж" + "," + "кв " + address.flat;
+    address = `ул. ${address.street},дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв. ${address.flat}`;
     data.order.address = address
     data.order.sum = sum;
     data.client = name + " " + phone;
